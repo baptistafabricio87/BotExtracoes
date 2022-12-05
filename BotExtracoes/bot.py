e75a54582ebd4726f7ee9efe1f81354cc38f9055
Fabricio Castro <baptistafabricio87@gmail.com>
2022-12-05 10:34:15 -0300

    Formatação com Black

--- BotExtracoes/bot.py
+++ BotExtracoes/bot.py
@@ -42,15 +42,17 @@ class Bot(DesktopBot):
         usuario = input("Informar Usuario SAP: ")
         senha = pwinput(prompt="Informar Senha SAP: ")
 
-        self.execute("C:\\Program Files (x86)\\SAP\\FrontEnd\\SAPgui\\saplogon.exe")
-        
-        if not self.find( 
+        self.execute(
+            "C:\\Program Files (x86)\\SAP\\FrontEnd\\SAPgui\\saplogon.exe"
+        )
+
+        if not self.find(
             "localiza_b05_pd4", matching=0.97, waiting_time=50000
         ):
             self.not_found("localiza_b05_pd4")
         self.double_click()
 
-        if not self.find( 
+        if not self.find(
             "localiza_usuario", matching=0.97, waiting_time=50000
         ):
             self.not_found("localiza_usuario")
@@ -64,18 +66,20 @@ class Bot(DesktopBot):
     def executa_sap_sp02(self):
         self.wait(2000)
         self.type_keys(["S", "P", "0", "2", "return"])
+        self.wait(1000)
         self.type_keys(["ctrl", "shift", "f10"])
+        self.wait(1000)
         self.type_keys(["tab", "tab", "tab", "delete", "return"])
 
     def salva_arquivo(self, nome_arquivo, caminho_diretorio, wait_time, me3l):
-        if not self.find( 
+        if not self.find(
             "btn_gravar_file_local", matching=0.97, waiting_time=wait_time
         ):
             self.not_found("btn_gravar_file_local")
         self.click()
 
         if not me3l:
-            if not self.find( 
+            if not self.find(
                 "selecionar_texto_com_tabuladores",
                 matching=0.97,
                 waiting_time=50000,
@@ -83,17 +87,17 @@ class Bot(DesktopBot):
                 self.not_found("selecionar_texto_com_tabuladores")
             self.click_relative(12, 9)
         else:
-            if not self.find( "nao_convert", matching=0.97, waiting_time=50000):
+            if not self.find("nao_convert", matching=0.97, waiting_time=50000):
                 self.not_found("nao_convert")
             self.click_relative(-8, 9)
 
-        if not self.find( 
+        if not self.find(
             "btn_confirmar_tipo_arquivo", matching=0.97, waiting_time=30000
         ):
             self.not_found("btn_confirmar_tipo_arquivo")
         self.click()
 
-        if not self.find( 
+        if not self.find(
             "localiza_diretorio", matching=0.97, waiting_time=wait_time
         ):
             self.not_found("localiza_diretorio")
@@ -106,14 +110,14 @@ class Bot(DesktopBot):
         self.tab(wait=100)
         self.tab(wait=100)
         self.space()
-        if not self.find( 
+        if not self.find(
             "confirma_arq_salvo", matching=0.97, waiting_time=wait_time
         ):
             self.not_found("confirma_arq_salvo")
         self.click_relative(248, -655)
 
     def extrair_acomp_pedido(self):
-        if not self.find( 
+        if not self.find(
             "localiza_acomp_pedido", matching=0.97, waiting_time=20000
         ):
             self.not_found("localiza_acomp_pedido")
@@ -127,7 +131,7 @@ class Bot(DesktopBot):
         )
 
     def extrair_po_contrato(self):
-        if not self.find( 
+        if not self.find(
             "localiza_po_contrato", matching=0.97, waiting_time=20000
         ):
             self.not_found("localiza_po_contrato")
@@ -141,7 +145,7 @@ class Bot(DesktopBot):
         )
 
     def extrair_ydv1(self):
-        if not self.find( "localiza_ydv1", matching=0.97, waiting_time=20000):
+        if not self.find("localiza_ydv1", matching=0.97, waiting_time=20000):
             self.not_found("localiza_ydv1")
         self.click_relative(-335, 4)
 
@@ -153,7 +157,7 @@ class Bot(DesktopBot):
         )
 
     def extrair_zmm_contrato(self):
-        if not self.find( 
+        if not self.find(
             "localiza_zmm_contrato", matching=0.97, waiting_time=20000
         ):
             self.not_found("localiza_zmm_contrato")
@@ -167,7 +171,7 @@ class Bot(DesktopBot):
         )
 
     def extrair_zmm_fornecedores(self):
-        if not self.find( 
+        if not self.find(
             "localiza_zmm_fornecedores", matching=0.97, waiting_time=20000
         ):
             self.not_found("localiza_zmm_fornecedores")
@@ -181,7 +185,7 @@ class Bot(DesktopBot):
         )
 
     def extrair_zmm_qualif_forn_rel(self):
-        if not self.find( 
+        if not self.find(
             "localiza_zmm_qualif", matching=0.97, waiting_time=30000
         ):
             self.not_found("localiza_zmm_qualif")
@@ -195,7 +199,7 @@ class Bot(DesktopBot):
         )
 
     def extrair_me3l(self):
-        if not self.find( "localiza_me3l", matching=0.97, waiting_time=30000):
+        if not self.find("localiza_me3l", matching=0.97, waiting_time=30000):
             self.not_found("localiza_me3l")
         self.click_relative(-334, 4)
 
@@ -209,25 +213,23 @@ class Bot(DesktopBot):
     def formatar_exportar_arquivos(self):
         app_path = "P:\Central_Abastecimento\Automações\Projeto de Contratos\Documentos\ExportarArquivos - Contratos.xlsm"
         self.execute(app_path)
-        if not self.find( "localiza_menu", matching=0.97, waiting_time=100000):
+        if not self.find("localiza_menu", matching=0.97, waiting_time=100000):
             self.not_found("localiza_menu")
         self.click()
-        if not self.find( "localiza_formatar_arquivos", matching=0.97, waiting_time=100000):
+        if not self.find(
+            "localiza_formatar_arquivos", matching=0.97, waiting_time=100000
+        ):
             self.not_found("localiza_formatar_arquivos")
         self.click()
-        if not self.find( "localiza_importar_arquivos", matching=0.97, waiting_time=600000):
+        if not self.find(
+            "localiza_importar_arquivos", matching=0.97, waiting_time=600000
+        ):
             self.not_found("localiza_importar_arquivos")
         self.click()
-        
-    
+
     def not_found(self, label):
         print(f"Element not found (Elemento nao encontrado): {label}")
 
 
 if __name__ == "__main__":
     Bot.main()
-
-
-
-
-
--- setup.py
+++ setup.py
@@ -4,17 +4,20 @@ from setuptools import setup, find_packages
 
 here = path.abspath(path.dirname(__file__))
 
-with open(path.join(here, 'README.md'), encoding='utf-8') as readme_file:
+with open(path.join(here, "README.md"), encoding="utf-8") as readme_file:
     readme = readme_file.read()
 
 
-with open(path.join(here, 'VERSION'), encoding='utf-8') as version_file:
+with open(path.join(here, "VERSION"), encoding="utf-8") as version_file:
     version = version_file.read()
 
-with open(path.join(here, 'requirements.txt')) as requirements_file:
+with open(path.join(here, "requirements.txt")) as requirements_file:
     # Parse requirements.txt, ignoring any commented-out lines.
-    requirements = [line for line in requirements_file.read().splitlines()
-                    if not line.startswith('#')]
+    requirements = [
+        line
+        for line in requirements_file.read().splitlines()
+        if not line.startswith("#")
+    ]
 
 
 setup(
@@ -22,16 +25,16 @@ setup(
     version=version,
     description="Bot para extracao de relatorios SAP para base de contratos",
     long_description=readme,
-    long_description_content_type='text/markdown',
-    packages=find_packages(exclude=['docs', 'tests']),
+    long_description_content_type="text/markdown",
+    packages=find_packages(exclude=["docs", "tests"]),
     include_package_data=True,
     package_data={
         "BotExtracoes": [
             # When adding files here, remember to update MANIFEST.in as well,
             # or else they will not be included in the distribution on PyPI!
             # 'path/to/data_file',
-            'resources',
+            "resources",
         ]
     },
-    install_requires='requirements',
+    install_requires="requirements",
 )
