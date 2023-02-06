16fa98100c4f0214882800dd3dc3f6eb5019713b
Fabricio Castro <baptistafabricio87@gmail.com>
2023-02-06 09:03:51 -0300

    UPDATE: Aumento dos tempos de espera.

--- BotExtracoes/bot.py
+++ BotExtracoes/bot.py
@@ -1,12 +1,14 @@
 from botcity.core import DesktopBot
-import pyautogui as pag
+import pyautogui
+
 
 class Bot(DesktopBot):
     def action(self, execution=None):
-        pag.alert(
+        pyautogui.alert(
             "INICIANDO PROCESSO DE EXTRAÇÕES \
                 \nInforme login e senha SAP e aguarde."
         )
+
         self.login_sap()
         self.executa_sap_sp02()
         print("Login OK! Processo Iniciado! Por favor aguarde...")
@@ -14,13 +16,14 @@ class Bot(DesktopBot):
         self.extrair_zmm_contrato()
         self.extrair_acomp_pedido()
         self.extrair_po_contrato()
-        self.extrair_ydv1()
         self.extrair_zmm_fornecedores()
+        self.extrair_ydv1()
         self.extrair_zmm_qualif_forn_rel()
         self.type_keys(["f3", "f3"])
         self.formatar_exportar_arquivos()
         print("Processo Finalizado!")
-        pag.alert(
+
+        pyautogui.alert(
             "PROCESSO CONCLUÍDO! \
                 \nFavor aguardar macros de formatação e importação"
         )
@@ -34,8 +37,8 @@ class Bot(DesktopBot):
             usuario (str): Usuario SAP
             senha (str): Senha SAP
         """
-        usuario = pag.prompt("Informar Usuario SAP ")
-        senha = pag.password("Informar Senha SAP ", mask="*")
+        usuario = pyautogui.prompt("Informar Usuario SAP ")
+        senha = pyautogui.password("Informar Senha SAP ", mask="*")
 
         self.execute(
             "C:\\Program Files (x86)\\SAP\\FrontEnd\\SAPgui\\saplogon.exe"
@@ -66,7 +69,9 @@ class Bot(DesktopBot):
         self.wait(1000)
         self.type_keys(["tab", "tab", "tab", "delete", "return"])
 
-    def salva_arquivo(self, nome_arquivo, caminho_diretorio, wait_time, convert):
+    def salva_arquivo(
+        self, nome_arquivo, caminho_diretorio, wait_time, convert
+    ):
         if not self.find(
             "btn_gravar_file_local", matching=0.97, waiting_time=wait_time
         ):
@@ -77,17 +82,17 @@ class Bot(DesktopBot):
             if not self.find(
                 "selecionar_texto_com_tabuladores",
                 matching=0.97,
-                waiting_time=50000,
+                waiting_time=500000,
             ):
                 self.not_found("selecionar_texto_com_tabuladores")
             self.click_relative(12, 9)
         else:
-            if not self.find("nao_convert", matching=0.97, waiting_time=50000):
+            if not self.find("nao_convert", matching=0.97, waiting_time=500000):
                 self.not_found("nao_convert")
             self.click_relative(-8, 9)
 
         if not self.find(
-            "btn_confirmar_tipo_arquivo", matching=0.97, waiting_time=30000
+            "btn_confirmar_tipo_arquivo", matching=0.97, waiting_time=300000
         ):
             self.not_found("btn_confirmar_tipo_arquivo")
         self.click()
@@ -98,12 +103,12 @@ class Bot(DesktopBot):
             self.not_found("localiza_diretorio")
         self.click()
 
-        self.tab(wait=100)
+        self.tab(wait=1000)
         self.paste(text=caminho_diretorio)
-        self.tab(wait=100)
+        self.tab(wait=1000)
         self.paste(text=nome_arquivo)
-        self.tab(wait=100)
-        self.tab(wait=100)
+        self.tab(wait=1000)
+        self.tab(wait=1000)
         self.space()
         if not self.find(
             "confirma_arq_salvo", matching=0.97, waiting_time=wait_time
@@ -120,87 +125,87 @@ class Bot(DesktopBot):
 
         self.salva_arquivo(
             convert=False,
-            wait_time=100000,
+            wait_time=1000000,
             nome_arquivo="Acomp_Pedido.txt",
             caminho_diretorio="P:\\Central_Abastecimento\\Automações\\Projeto de Contratos\\Parametros\\",
         )
 
     def extrair_po_contrato(self):
         if not self.find(
-            "localiza_po_contrato", matching=0.97, waiting_time=20000
+            "localiza_po_contrato", matching=0.97, waiting_time=200000
         ):
             self.not_found("localiza_po_contrato")
         self.click_relative(-336, 2)
 
         self.salva_arquivo(
             convert=False,
-            wait_time=40000,
+            wait_time=4000000,
             nome_arquivo="POContrato.txt",
             caminho_diretorio="P:\\Central_Abastecimento\\Automações\\Projeto de Contratos\\Parametros\\",
         )
 
     def extrair_ydv1(self):
-        if not self.find("localiza_ydv1", matching=0.97, waiting_time=20000):
+        if not self.find("localiza_ydv1", matching=0.97, waiting_time=200000):
             self.not_found("localiza_ydv1")
         self.click_relative(-335, 4)
 
         self.salva_arquivo(
             convert=False,
-            wait_time=10000,
+            wait_time=100000,
             nome_arquivo="YDV1.txt",
             caminho_diretorio="P:\\Central_Abastecimento\\Automações\\Projeto de Contratos\\Parametros\\",
         )
 
     def extrair_zmm_contrato(self):
         if not self.find(
-            "localiza_zmm_contrato", matching=0.97, waiting_time=20000
+            "localiza_zmm_contrato", matching=0.97, waiting_time=200000
         ):
             self.not_found("localiza_zmm_contrato")
         self.click_relative(-334, 6)
 
         self.salva_arquivo(
             convert=False,
-            wait_time=1000000,
+            wait_time=10000000,
             nome_arquivo="ZMM_contratos.txt",
             caminho_diretorio="P:\\Central_Abastecimento\\Automações\\Projeto de Contratos\\Parametros\\",
         )
 
     def extrair_zmm_fornecedores(self):
         if not self.find(
-            "localiza_zmm_fornecedores", matching=0.97, waiting_time=20000
+            "localiza_zmm_fornecedores", matching=0.97, waiting_time=200000
         ):
             self.not_found("localiza_zmm_fornecedores")
         self.click_relative(-333, 4)
 
         self.salva_arquivo(
             convert=False,
-            wait_time=50000,
+            wait_time=500000,
             nome_arquivo="ZMM_fornecedores.txt",
             caminho_diretorio="P:\\Central_Abastecimento\\Automações\\Projeto de Contratos\\Parametros\\",
         )
 
     def extrair_zmm_qualif_forn_rel(self):
         if not self.find(
-            "localiza_zmm_qualif", matching=0.97, waiting_time=30000
+            "localiza_zmm_qualif", matching=0.97, waiting_time=300000
         ):
             self.not_found("localiza_zmm_qualif")
         self.click_relative(-335, 4)
 
         self.salva_arquivo(
             convert=False,
-            wait_time=30000,
+            wait_time=300000,
             nome_arquivo="ZMM_qualif_forn_rel.txt",
             caminho_diretorio="P:\\Central_Abastecimento\\Automações\\Projeto de Contratos\\Parametros\\",
         )
 
     def extrair_me3l(self):
-        if not self.find("localiza_me3l", matching=0.97, waiting_time=30000):
+        if not self.find("localiza_me3l", matching=0.97, waiting_time=300000):
             self.not_found("localiza_me3l")
         self.click_relative(-334, 4)
 
         self.salva_arquivo(
             convert=True,
-            wait_time=600000,
+            wait_time=6000000,
             nome_arquivo="ME3L.txt",
             caminho_diretorio="P:\\Central_Abastecimento\\Automações\\Projeto de Contratos\\Parametros\\",
         )
@@ -208,16 +213,16 @@ class Bot(DesktopBot):
     def formatar_exportar_arquivos(self):
         app_path = "P:\Central_Abastecimento\Automações\Projeto de Contratos\Documentos\ExportarArquivos - Contratos.xlsm"
         self.execute(app_path)
-        if not self.find("localiza_menu", matching=0.97, waiting_time=100000):
+        if not self.find("localiza_menu", matching=0.97, waiting_time=1000000):
             self.not_found("localiza_menu")
         self.click()
         if not self.find(
-            "localiza_formatar_arquivos", matching=0.97, waiting_time=100000
+            "localiza_formatar_arquivos", matching=0.97, waiting_time=1000000
         ):
             self.not_found("localiza_formatar_arquivos")
         self.click()
         if not self.find(
-            "localiza_importar_arquivos", matching=0.97, waiting_time=600000
+            "localiza_importar_arquivos", matching=0.97, waiting_time=6000000
         ):
             self.not_found("localiza_importar_arquivos")
         self.click()
